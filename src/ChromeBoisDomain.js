53682c29a141192f24e29f7f00ff5fac653490da
Tammy Metz <tammy@tammymetz.com>
2019-10-22 02:15:05 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -10,7 +10,7 @@ export default class ChromeBoisDomain extends Component {
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
-     this.drawChromeBoiAtCoords(event.screenX, event.screenY)
+     drawChromeBoiAtCoords(event.screenX, event.screenY)
   }
 
   /* TODO: Create an event handler which, when fired, invokes the provided
